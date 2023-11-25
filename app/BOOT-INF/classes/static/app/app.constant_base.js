acf8a5ea35c2d8ca9790181c9a04479dfa8b4d1e
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2023-11-25 17:24:33 +0000

    :fire: changed repository app files

---
+++ app/BOOT-INF/classes/static/app/app.constant_base.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('imnApp')
+        .constant('API_CONFIG', {
+            uri: 'http://@',
+            apiPort: ':8080'
+        });
+})();
